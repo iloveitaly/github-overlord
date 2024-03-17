392336f24a4c7f3440ec47d44da2a8650b8f68b4
Michael Bianco <iloveitaly@gmail.com>
2024-03-17 11:58:38 -0600

    dry run

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import os
 import logging
+import click
 from github import Github
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 
-def merge_pr(pr):
-    pr.create_issue_comment("This PR was automatically merged with dependabot-overlord")
-    pr.merge(merge_method="squash")
-    logging.info(f"Merged PR: {pr.html_url}")
+def merge_pr(pr, dry_run):
+    if dry_run:
+        logging.info(f"Would merge PR: {pr.html_url}")
+    else:
+        pr.create_issue_comment(
+            "This PR was automatically merged with dependabot-overlord"
+        )
+        pr.merge(merge_method="squash")
+        logging.info(f"Merged PR: {pr.html_url}")
 
 
 def is_eligible_for_merge(pr_title):
     return "patch" in pr_title.lower() or "github actions" in pr_title.lower()
 
 
-def main():
-    token = os.getenv("GITHUB_TOKEN")
+def main(token, dry_run):
     g = Github(token)
 
     user = g.get_user()
@@ -32,7 +37,7 @@ def main():
                     commit = pr.get_commits().reversed[0]
                     status = commit.get_combined_status().state
                     if status == "success":
-                        merge_pr(pr)
+                        merge_pr(pr, dry_run)
                     else:
                         logging.info(
                             f"Skipping PR (checks not successful): {pr.html_url}"
@@ -41,5 +46,12 @@ def main():
                     logging.info(f"Skipping PR (not eligible): {pr.html_url}")
 
 
+@click.command()
+@click.option("--dry-run", is_flag=True, help="Run script without making changes")
+def cli(dry_run):
+    token = os.getenv("GITHUB_TOKEN")
+    main(token, dry_run)
+
+
 if __name__ == "__main__":
-    main()
+    cli()
